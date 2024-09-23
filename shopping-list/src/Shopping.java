911af7e817437a1a63b001f4866b1971ae45f408
StefKot <96449266+StefKot@users.noreply.github.com>
2024-09-23 11:55:28 +0300

    Update Shopping.java

--- shopping-list/src/Shopping.java
+++ shopping-list/src/Shopping.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
 
 public class Shopping {
 
@@ -6,8 +9,7 @@ public class Shopping {
 
         System.out.println("Вас приветствует список покупок!");
 
-        String[] shoppingList = new String[8]; // Объявите массив shoppingList через длину
-        int productCount = 0; // переменная для подсчёта добавленных товаров
+        ArrayList<String> shoppingList = new ArrayList<>(8);
 
         Scanner scanner = new Scanner(System.in);
 
@@ -17,14 +19,41 @@ public class Shopping {
             System.out.println("2. Показать список");
             System.out.println("3. Очистить список");
             System.out.println("4. Завершить работу");
+            System.out.print("--> ");
 
-            int actionNumber = scanner.nextInt();
+            String actionNumber = scanner.next();
 
-            if (actionNumber == 1) {
-               ... //добавить товар
+            if (actionNumber.equals("1")) {
+                System.out.println("Введите название товара: "); //добавить товар
+                System.out.print("--> ");
+                String productName = scanner.next();
+                if (shoppingList.contains(productName)) {
+                    System.out.println("Товар уже есть в списке");
+                }
+                else {
+                    shoppingList.add(productName);
+                    System.out.println("Товар " + productName + " добавлен в список под номером " + shoppingList.size());
+                }
             }
-            ... // обработка остальных действий
-            else if (actionNumber == 4) {
+            else if (actionNumber.equals("2")) {
+                if (shoppingList.isEmpty()) {
+                    System.out.println("Список товаров пуст");
+                } else {
+                    System.out.println("Список товаров: "); // обработка остальных действий
+                    for (int i = 0; i < shoppingList.size(); i++) {
+                        System.out.println(i + 1 + ". " + shoppingList.get(i));
+                    }
+                }
+
+            }
+            else if (actionNumber.equals("3")) {
+                /*for (int i = shoppingList.size() - 1; i >= 0; i--) {
+                    shoppingList.remove(i);
+                }*/
+                shoppingList.clear();
+                System.out.println("Список товаров очищен");
+            }
+            else if (actionNumber.equals("4")) {
                 break;
             } else {
                 System.out.println("Неизвестная команда!");
