226e8d7620406df3a79572a74e89dc4f34c6d19e
StefKot <StefanKotov22@gmail.com>
2024-10-05 19:40:31 +0300

    variable has been changed

--- java-sprint2-hw/src/Converter.java
+++ java-sprint2-hw/src/Converter.java
@@ -2,10 +2,10 @@ public class Converter {
     int stepLength = 75;
     int stepEnergy = 50;
     int kcal = 1000;
-    int cmToKm = 100000;
+    int kmToCm = 100000;
 
     int convertToKm(int steps) {
-        return steps * stepLength / cmToKm;
+        return steps * stepLength / kmToCm;
     }
 
     int convertStepsToKilocalories(int steps) {
