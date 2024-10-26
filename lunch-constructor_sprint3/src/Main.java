a2b891ef15eead451893a193eb17c3ef461c11c7
StefKot <96449266+StefKot@users.noreply.github.com>
2024-10-26 17:36:53 +0300

    Working in progress

--- lunch-constructor_sprint3/src/Main.java
+++ lunch-constructor_sprint3/src/Main.java
@@ -1,2 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        DinnerConstructor dinnerConstructor = new DinnerConstructor();
+
+
+        while (true) {
+            printMenu();
+            int command = scanner.nextInt();
+
+            if (command == 1) {
+                System.out.println("Введите тип блюда: ");
+                String dishType = scanner.next();
+                System.out.println("Введите название блюда: ");
+                String dishName = scanner.next();
+                dinnerConstructor.addNewDish(dishType, dishName);
+            } else if (command == 2) {
+                dinnerConstructor.generateCombinationOfDishes();
+            } else if (command == 3) {
+                System.out.println("Выход");
+                break;
+            } else {
+                System.out.println("Извините, такой команды пока нет.");
+            }
+        }
+    }
+
+    public static void printMenu() {
+        System.out.println("Выберите команду:");
+        System.out.println("1 - Добавить новое блюдо");
+        System.out.println("2 - Сгенерировать комбинации блюд");
+        System.out.println("3 - Выход");
+    }
 }
